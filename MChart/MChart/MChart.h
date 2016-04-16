7c0b77c07e4947a875205d5b5d9b52abcaaa8839
boguang <boguang@b5m.com>
2016-04-16 23:53:45 +0800

    init project

---
+++ MChart/MChart/MChart.h
@@ -0,0 +1,19 @@
+//
+//  MChart.h
+//  MChart
+//
+//  Created by Micker on 16/4/16.
+//  Copyright © 2016年 micker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MChart.
+FOUNDATION_EXPORT double MChartVersionNumber;
+
+//! Project version string for MChart.
+FOUNDATION_EXPORT const unsigned char MChartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MChart/PublicHeader.h>
+
+
